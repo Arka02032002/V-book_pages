47eb5f621df9acf44aa80715b84ec47c4ded8153
Arkatanu Kundu <arkatanukundu@gmail.com>
2022-04-02 16:23:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import Caraousel from "./components/Caraousel";
+import Contact2 from"./components/Contact2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="main container">
+        <Contact />
+        <Caraousel />
+        <Contact2 />
+      </div>
+      <div className="feedback container">
+        <p>Please share your feedback and other queries</p>
+        <a href="/">Google form link</a>
+      </div>
+    </>
   );
 }
 
